389e4700c170bfd990458e8136fbd406be83d5f1
nachospada <44822250+nachospada@users.noreply.github.com>
2018-11-07 15:47:34 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -23,15 +23,7 @@ function toggleMenu() {
         
         document.getElementsByClassName('btn-line')[0].style.transform = "rotate(45deg) translate(5px, 5px)";
         document.getElementsByClassName('btn-line')[1].style.display = "none";
-        document.getElementsByClassName('btn-line')[2].style.transform = "rotate(-45deg) translate(1px, -1px)";
-        
-        menuItems.children[0].style.animationName = "fromRight";
-        menuItems.children[0].style.animationDuration = ".5s";
-        
-        menuItems.children[1].style.animationName = "fromRight";
-        menuItems.children[1].style.animationDuration = ".8s";
-        
-               
+        document.getElementsByClassName('btn-line')[2].style.transform = "rotate(-45deg) translate(1px, -1px)";        
     } else {
         menuBtn.classList.remove('close'); 
         menu.classList.remove('show'); 
@@ -39,18 +31,11 @@ function toggleMenu() {
         menuRight.classList.remove('show'); 
         menuItems.classList.remove('show');
         
-        menuItems.children[0].style.animationName = "toRight";
-        menuItems.children[0].style.animationDuration = ".5s";
-        
-        menuItems.children[1].style.animationName = "toRight";
-        menuItems.children[1].style.animationDuration = ".8s";
-        
         //SET MENU STATE
         showMenu = false;
         
         document.getElementsByClassName('btn-line')[0].style.transform = "none";
         document.getElementsByClassName('btn-line')[1].style.display = "block";
-        document.getElementsByClassName('btn-line')[2].style.transform = "none";  
-        
+        document.getElementsByClassName('btn-line')[2].style.transform = "none";          
     }
 }
