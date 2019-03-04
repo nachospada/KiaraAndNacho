9995915aa49c9b2311651d6bbbcafd9713480807
Nacho Spadaccini <44822250+nachospada@users.noreply.github.com>
2019-03-04 10:14:04 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const menu = document.querySelector('.menu');
 const menuLeft = document.querySelector('.menu-left');
 const menuRight = document.querySelector('.menu-right');
 const menuItems = document.querySelector('.menu-items');
+const dateDisplay = document.querySelector('.dateDisplay');
 
 // Set Initial State of the Menu
 let showMenu = false;
@@ -33,12 +34,16 @@ function toggleMenu() {
         menuRight.classList.add('show'); 
         menuItems.classList.add('show');
         
+        dateDisplay.style.opacity = "0";
+//        dateDisplay.style.transform = "translate3d(-80%,0,0)";
+        
         //SET MENU STATE
         showMenu = true; 
         
         document.getElementsByClassName('btn-line')[0].style.transform = "rotate(45deg) translate(5px, 5px)";
         document.getElementsByClassName('btn-line')[1].style.opacity = "0";
-        document.getElementsByClassName('btn-line')[2].style.transform = "rotate(-45deg) translate(6px, -7px)";        
+        document.getElementsByClassName('btn-line')[2].style.transform = "rotate(-45deg) translate(6px, -7px)"; 
+
     } else {
         menuBtn.classList.remove('close'); 
         menu.classList.remove('show'); 
@@ -46,11 +51,50 @@ function toggleMenu() {
         menuRight.classList.remove('show'); 
         menuItems.classList.remove('show');
         
+        dateDisplay.style.opacity = "1";
+//        dateDisplay.style.transform = "translate3d(0,0,0)";
+        
         //SET MENU STATE
         showMenu = false;
         
         document.getElementsByClassName('btn-line')[0].style.transform = "none";
         document.getElementsByClassName('btn-line')[1].style.opacity = "1";
-        document.getElementsByClassName('btn-line')[2].style.transform = "none";          
+        document.getElementsByClassName('btn-line')[2].style.transform = "none";   
+        
     }
 }
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 8, 2020 15:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+//  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+//  + minutes + "m " + seconds + "s ";
+    
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours; 
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
